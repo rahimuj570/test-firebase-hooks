b7f3a72a2846f54d746692399a7d36d805290fe9
rahimuj570 <rahimuj570@gmail.com>
2022-04-12 21:10:03 +0600

    signup successfully

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -9,16 +9,16 @@ const Login = () => {
           <div className="mb-4">
             <label
               className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="username"
+              htmlFor="Email"
             >
-              Username
+              Email
             </label>
             <input
               required
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="username"
+              id="Email"
               type="text"
-              placeholder="Username"
+              placeholder="abc@email.com"
             />
           </div>
           <div className="mb-6">
--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,24 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import auth from "./firebase.int";
 
 const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirm_password, setConfirm_Password] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
+
+  const [createUserWithEmailAndPassword, user, loading, error] =
+    useCreateUserWithEmailAndPassword(auth);
+
   return (
     <div>
       <div className="mx-auto mt-10 w-full max-w-xs">
-        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            if (password !== confirm_password) {
+              setErrorMsg("Confirm Password is not Same");
+              return;
+            }
+            if (password.length < 6) {
+              setErrorMsg("Password Must be Contain Minimum 6 Character");
+              return;
+            }
+            createUserWithEmailAndPassword(email, password);
+            console.log(email, password);
+            setErrorMsg("");
+          }}
+          className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        >
           <div className="mb-4">
             <label
               className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="username"
+              htmlFor="Email"
             >
-              Username
+              Email
             </label>
             <input
+              onBlur={(e) => setEmail(e.target.value)}
               required
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="username"
-              type="text"
-              placeholder="Username"
+              id="Email"
+              type="email"
+              placeholder="abc@email.com"
             />
           </div>
           <div className="mb-6">
@@ -29,6 +57,7 @@ const Signup = () => {
               Password
             </label>
             <input
+              onBlur={(e) => setPassword(e.target.value)}
               className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
               id="password"
               type="password"
@@ -41,14 +70,15 @@ const Signup = () => {
               Confirm Password
             </label>
             <input
+              onBlur={(e) => setConfirm_Password(e.target.value)}
               className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
               id="confirm-password"
               type="password"
               placeholder="******************"
             />
-            <p className="text-red-500 text-xs italic">
-              Please choose a password.
-            </p>
+            {errorMsg && (
+              <p className="text-red-500 text-xs italic">{errorMsg}</p>
+            )}
           </div>
           <div className="flex items-center justify-between">
             <button
